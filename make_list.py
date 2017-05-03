034dcadc1cc6f0bdd65ed6ca351bd77b88c1f6ba
david work vm <lloydyc@gmail.com>
2017-05-03 10:03:46 +0100

    better checking for DateTime tag

--- make_list.py
+++ make_list.py
@@ -7,37 +7,52 @@ from PIL.ExifTags import TAGS, GPSTAGS
 
 
 def get_field (exif,field) :
-  for (k,v) in exif.iteritems():
-     if TAGS.get(k) == field:
-        return v
+    #print exif
+    for (k,v) in exif.iteritems():
+        if TAGS.get(k) == field:
+            return v
 
 
 print "hello"
 
-dir_root = "/home/david/Downloads"
+dir_root = "/home/david"
 
 
 f = []
 for (root, dirs, files) in os.walk(dir_root):
     for name in files:
         fullname = (os.path.join(root, name))
-        print fullname
+        #if '/.cache/' in fullname:
+        #    continue
+        #print fullname
         try:
             img = Image.open(fullname)
+   
+            
         except:
-            print "  not image"
+            #print "  not image"
             continue
-
+        #print fullname
+        #print img
 
         #print(img)
         try:
             info = img._getexif()
+            #print info
         except:
-            print "  no exif"
+            #print "  no exif"
             continue
-
+        if info:
+            dt = get_field(info, 'DateTime')
+        else:
+            continue
+        
+        
         #for (k, v) in info.iteritems():
         #    print '  %s = %s' % (TAGS.get(k), v)
 
         #print info
-        print "  %s" % (get_field(info, 'DateTime') )
+        print "%s  %s" % (fullname , dt)
+        
+        
+        
